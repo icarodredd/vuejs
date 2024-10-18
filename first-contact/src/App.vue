a5aa46c65fa0295beb93595782a9e8142c705038
icarodredd <icarog854@gmail.com>
2024-10-18 15:24:28 -0300

    attribute binding

--- first-contact/src/App.vue
+++ first-contact/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const one = 1
 const someHtml = "<span style='color: red'>This should be red.</span>"
+const someClass = 'someClass'
 </script>
 
 <template>
@@ -8,6 +9,7 @@ const someHtml = "<span style='color: red'>This should be red.</span>"
     <p>hello world from vue!</p>
     <p>this is a number: {{ one }}</p>
     <p>someHtml: <span v-html="someHtml"></span></p>
+    <p :id="someClass">My class is: {{ someClass }}</p>
   </main>
 </template>
 
