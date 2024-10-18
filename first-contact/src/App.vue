77258d5a7ce709f1ab6239d6c91d521dc544404e
icarodredd <icarog854@gmail.com>
2024-10-18 15:17:29 -0300

    rawHTML

--- first-contact/src/App.vue
+++ first-contact/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 const one = 1
+const someHtml = "<span style='color: red'>This should be red.</span>"
 </script>
 
 <template>
   <main>
     <p>hello world from vue!</p>
     <p>this is a number: {{ one }}</p>
+    <p>someHtml: <span v-html="someHtml"></span></p>
   </main>
 </template>
 
